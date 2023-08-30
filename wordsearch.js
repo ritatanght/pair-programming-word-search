7b3634015f96c43ad685d4598656fec1119807a2
Rita Tang <doggymint@gmail.com>
2023-08-30 18:12:21 -0400

    add logic for vertical match, add testcases

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) return false;
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  const verticalJoin = transpose(letters).map((ls) => ls.join(""));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (const l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+const transpose = function(letters) {
+  let newArray = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    newArray.push([]);
+  }
+
+  for (let x = 0; x < letters.length; x++) {
+    for (let y = 0; y < letters[0].length; y++) {
+      newArray[y][x] = letters[x][y];
+    }
+  }
+  return newArray;
+};
+
+module.exports = wordSearch;
